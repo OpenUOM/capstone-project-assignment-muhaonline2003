8e34c1cbe7fe7081a340558f1b12b82f9172469e
muhaonline2003 <muhaonline2003@gmail.com>
2023-04-17 01:38:52 -0700

    database minor changes

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -51,13 +51,13 @@ export class StudentTableComponent implements OnInit {
     })
   }
 
-  search(value) {
+  search(value: string) {
     let foundItems = [];
     if (value.length <= 0) {
       this.getStudentData();
     } else {
       let b = this.studentData.filter((student) => {
-        if (student[0].name.toLowerCase().indexOf(value) > -1) {
+        if (student[0].name.toLowerCase().indexOf(value.toLowerCase()) > -1) {
           foundItems.push(student)
         }
       });
